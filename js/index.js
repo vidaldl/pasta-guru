ceab1dc661f659bfc09c4f466a8d4b5a3ffe7b66
Leo Vidal <vidaldl@hotmail.com>
2024-12-07 21:11:47 -0700

    Revert "Feat/add dynamic recipes"

--- js/index.js
+++ js/index.js
@@ -1,16 +1,1 @@
 //Main JS file
-
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('data/recipes.json')
-    .then(response => response.json())
-    .then(recipes => {
-      const recipeList = document.getElementById('recipe-list');
-      recipes.forEach(recipe => {
-        const listItem = document.createElement('li');
-        listItem.className = 'list-group-item';
-        listItem.innerHTML = `${recipe.name} <a href="recipe.html?id=${recipe.id}" class="btn btn-primary btn-sm">View Recipe</a>`;
-        recipeList.appendChild(listItem);
-      });
-    })
-    .catch(error => console.error('Error fetching recipes:', error));
-});
